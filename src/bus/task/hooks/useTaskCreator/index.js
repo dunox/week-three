94bcbf4a5e327a56f3bf5e64dc88bf1684fc653d
Andriy <34597632+dunox@users.noreply.github.com>
2020-09-30 12:41:26 +0300

    edit task creator

--- src/bus/task/hooks/useTaskCreator/index.js
+++ src/bus/task/hooks/useTaskCreator/index.js
@@ -1,12 +1,37 @@
 // Core
 import { loader } from 'graphql.macro';
 import { useMutation } from '@apollo/react-hooks';
+import { useHistory } from 'react-router-dom';
 
+// Book
+import { book } from '../../../../navigation/book';
 
 // Mutations
 const mutationTaskCreator = loader('./gql/mutationTaskCreator.graphql');
 
 export const useTaskCreator = () => {
-    const [ createTask, {  error } ] = useMutation(mutationTaskCreator);
-    return { createTask, error};
+    const [ _addTask, {error} ] = useMutation(mutationTaskCreator);
+    const history = useHistory();
+
+    const createTask = async (task, refetch) => {
+        try {
+            const {data} = await _addTask({
+                variables: {
+                    task
+                }
+            });
+
+            if (data) {
+                history.push(book.tasks);
+            }
+        }
+        catch (error) {
+            console.log(error.message);
+        }
+        refetch();
+    };
+    return {
+        error,
+        createTask,
+    }
 }
